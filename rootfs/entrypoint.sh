16b57e64f7694a8023c23437cd2ef7d1c2cf381a
Andrey Artamonychev <me@andrey.wtf>
2023-09-24 21:43:23 +0300

    use github url because of CF blockage

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,14 +3,25 @@ set -e
 
 mount --make-rshared /usr/share/hassio
 
-cd /tmp
-wget https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_x86_64.deb
-wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
 
-apt install ./os-agent_1.6.0_linux_x86_64.deb
+cd /tmp
+
+git clone https://github.com/home-assistant/supervised-installer.git
+cd supervised-installer
+git apply /mypatch.patch
+docker run --rm -v $(pwd):/tmp debian:bullseye-slim bash -c \
+          "cd /tmp \
+          && chmod 555 homeassistant-supervised/DEBIAN/p* \
+          && dpkg-deb --build --root-owner-group homeassistant-supervised"
+cd ..
+
+
+wget https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_x86_64.deb
+
+apt install -y ./os-agent_1.6.0_linux_x86_64.deb
 
 rm -rf /etc/docker/*
 
-apt install ./homeassistant-supervised.deb
+apt install -y ./supervised-installer/homeassistant-supervised.deb
 
 exec "$@"
