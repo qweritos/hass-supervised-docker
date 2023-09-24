ee5490175043ffd1ef28d9d0ad75b671de926a18
Qweritos <me@andrey.wtf>
2023-09-24 19:08:23 +0000

    Update 'rootfs/entrypoint.sh'

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -6,7 +6,8 @@ mount --make-rshared /usr/share/hassio
 
 cd /tmp
 
-git clone https://github.com/home-assistant/supervised-installer.git
+rm -rf supervised-installer
+git clone https://github.com/home-assistant/supervised-installer.git supervised-installer
 cd supervised-installer
 git apply /mypatch.patch
 docker run --rm -v $(pwd):/tmp debian:bullseye-slim bash -c \
@@ -15,10 +16,9 @@ docker run --rm -v $(pwd):/tmp debian:bullseye-slim bash -c \
           && dpkg-deb --build --root-owner-group homeassistant-supervised"
 cd ..
 
+wget -O os-agent_linux_x86_64.deb https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_x86_64.deb
 
-wget https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_x86_64.deb
-
-apt install -y ./os-agent_1.6.0_linux_x86_64.deb
+apt install -y ./os-agent_linux_x86_64.deb
 
 rm -rf /etc/docker/*
 
